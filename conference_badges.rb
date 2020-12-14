62b37387c05b199f8abe32c73bb7cd49fa921cb7
Reynelle <Rtalford13@gmail.com>
2020-12-14 15:49:26 -0500

    commit tree 566b5d3e4923e95477d731f685c4b5194263c5dd

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(speakers)
+    array = []
+    speakers.each { |name| array << badge_maker(name) }
+    return array 
+ end
+
+ def assign_rooms(speakers)
+    array = []
+    speakers.each_with_index do |name, index|
+    array << "Hello, #{name}! You'll be assigned to room #{index+1}!" 
+    end
+    return array
+ end
+
+ def printer(speakers)
+   batch_badge_creator(speakers).each { |names| puts names }
+   assign_rooms(speakers).each { |room| puts room }
+end
